49c7912f8a0e9002a43b967f73e6778b1c2bac59
Kamarul Khairi <kamarulkhairi@gmail.com>
2020-11-09 18:05:33 +0800

    update question 4c

--- Question4c.py
+++ Question4c.py
@@ -1,16 +1,13 @@
-a = 76
-b = 3
-
-
 def add_numbers(a, b):
     return a + b
 
 
-def decoration_function(a, b):
-    return (2 * (3 + 4))
+def print_arguments(augmented_add_number1, augmented_add_number2):
+    print("Arguments are: ", augmented_add_number1, augmented_add_number2, " and will return ",
+          2 * (add_numbers(augmented_add_number1, augmented_add_number2)))
+    return 2 * (add_numbers(augmented_add_number1, augmented_add_number2))
 
 
-print(add_numbers(3, 4))
-x = decoration_function(3, 4)
-print(decoration_function(a, b))
-print("Arguments are: " + str(a) + ", " + str(b))
+augmented_add_numbers_a = int(input("Enter your numbers"))
+augmented_add_numbers_b = int(input("Enter your numbers"))
+print_arguments(augmented_add_numbers_a, augmented_add_numbers_b)
